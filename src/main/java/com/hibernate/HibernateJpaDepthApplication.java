a153ca661ddd1d7427a76aa5ca7e79898b1e3fd2
Yogesh Deshmukh <sampoorna.mahanti@tcs.com>
2020-09-11 08:44:00 +0530

    Initial commit

---
+++ src/main/java/com/hibernate/HibernateJpaDepthApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaDepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaDepthApplication.class, args);
+	}
+
+}
